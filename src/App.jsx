f2a96a8a2562c195c8e82217a51fd12527bafcda
Gabriela Araujo <gabyara237@gmail.com>
2026-01-22 09:32:15 -0800

    feat: fetch and provide recipes data in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import RecipeList from './components/Homepage/Homepage';
+import * as recipeService from './services/recipeService/recipeService'
 
 import './App.css'
 
+import { Route, Routes } from 'react-router';
+import { useEffect, useState } from 'react';
+
 const App = () => {
-  return <h1>Hello world!</h1>;
+
+  const [recipes, setRecipes] = useState([])
+console.log('App rendered');
+
+  useEffect(()=>{
+    const fetchAllRecipes = async () =>{
+      const recipesData = await recipeService.index();
+      setRecipes(recipesData)
+    }
+    fetchAllRecipes()
+  },[])
+
+  return(
+    <>
+      <Routes>
+        <Route path='/recipes' element={<RecipeList recipes={recipes}/>}/>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
