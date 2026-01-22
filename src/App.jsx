fc227d146b5ac1cfd3c96fcd6e5c06d89d23feff
Rebecca <beckie04@gmail.com>
2026-01-22 11:39:47 -0800

    feat: add basic navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 
-import './App.css'
+
+import NavBar from './components/NavBar/NavBar';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  
+  return (
+    <>
+      <NavBar />
+      <h1>Hello, friend!</h1>
+    </>
+  );
 };
 
 export default App;
+
