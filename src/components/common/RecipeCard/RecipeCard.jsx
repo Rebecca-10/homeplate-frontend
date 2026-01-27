0b0a3b988a465c6cd363b610575f13fdb53d8821
Gabriela Araujo <gabyara237@gmail.com>
2026-01-27 14:19:49 -0800

    feat: finalize RecipeCard layout and styling

--- src/components/common/RecipeCard/RecipeCard.jsx
+++ src/components/common/RecipeCard/RecipeCard.jsx
@@ -1,6 +1,13 @@
 import { useContext, useState } from "react";
 import { Link } from "react-router";
 
+import timeImage from '../../../assets/time.png'
+import servingImage from '../../../assets/servings.png'
+import heartImage from '../../../assets/heart.png';
+import heartRedImage from '../../../assets/heartRed.png'
+import commentImage from '../../../assets/comment.png'
+
+import './recipeCard.css'
 
 import { UserContext } from '../../../contexts/UserContext';
 
@@ -49,23 +56,71 @@ const RecipeCard=({recipe,toggleLike, followingIds, handleFollow})=>{
     }
 
     return(
-        <div key={recipe._id}> 
-            <div>
-                <p>{recipe.author.username}</p>
-                <button onClick={()=>handleChangeFollow(authorId)}>{followed? "Unfollow":"Follow"}</button>
+        <div className="main-container" key={recipe._id}> 
+            <div className="publication-info">
+                <div className="username">
+                    <p className="username-initial">{recipe.author.username.charAt(0).toUpperCase()}</p>
+                    <p>{recipe.author.username}</p>
+
+                </div>
+                {
+                    (recipe.author._id !== user._id)? <button className="btn-follow" onClick={()=>handleChangeFollow(authorId)}>{followed? "Unfollow": <><span className="plus"> + </span> Follow</>}</button> :<></>
+                }
+                
             </div>
-            <Link to = {`/recipes/${recipe._id}`}>
-                <article>
-                    <header>
-                        <h2>{recipe.title}</h2>
-                        <p>{`posted on ${new Date(recipe.createdAt).toLocaleDateString()}`}</p>
-                    </header>
-                        <p>{recipe.description}</p>
-                </article>
-            </Link>
-            <div>
-                <button onClick={handleChangeLike}>{liked? "Unlike":"Like"}</button> 
-                <p>{likesCount}</p>
+            <div className="card-info-container">
+                <Link className="card-info-container" to = {`/recipes/${recipe._id}`}>
+                <div className="left-container">
+                    <img className="recipe-image" src={recipe.imageUrl} alt={recipe.title}/>
+                </div>
+                <div className="right-container">
+                    <div className="recipe-info">
+                        <div className="header-info" >
+                            <h2>{recipe.title}</h2>
+                        </div>
+                        <div className="recipe-type">
+                            <p className="type">{recipe.typeRecipe}</p>
+                        </div>
+                        <div className="time-serving-info">
+                            <div className="time-info">
+                                <img  className="icon" src={timeImage} alt={recipe.title} />
+                                <p>{recipe.prepTime} min </p>
+
+                            </div>
+                            <div className="serving-info">
+                                <img className="icon" src={servingImage} alt={recipe.title} />
+                                <p> {recipe.servings} servings </p>
+                            </div>
+                            
+                            
+                        </div>
+                        <div className="recipe-description">
+                            <p>{recipe.description}</p>
+                        </div>
+                        <div className="tags">
+                            {
+                                recipe.tags.map((tag,index)=>(
+                                    <p className="tag" key={index}>#{tag}</p>
+                                    
+                                ))
+                            }
+                        </div>
+
+                    </div>
+
+                </div>
+                </Link>
+
+            </div>
+            <div className="interaction-recipes">
+                <div className="likes">
+                    <p>{likesCount}</p>
+                    <button className="btn-like" onClick={handleChangeLike}>{liked? <img  className="icon2" src={heartRedImage} alt="unlike" />:<img  className="icon2" src={heartImage} alt="like" />}</button> 
+                </div>
+                <div className="comments">
+                    <p>{recipe.comments.length}</p>
+                    <img  className="icon" src={commentImage} alt="comment" />
+                </div>
             </div>
         </div>
     )
