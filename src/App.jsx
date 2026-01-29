9a8ef34f8acb41c11a6eadfae76817aa737a272a
Gabriela Araujo <gabyara237@gmail.com>
2026-01-29 15:51:44 -0800

    feat: show only user-created recipes in my recipes view

--- src/App.jsx
+++ src/App.jsx
@@ -147,7 +147,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<RecipeList recipes={recipes} toggleLike={toggleLike} followingIds={followingIds} handleFollow={handleFollow}/>} />
           <Route path="/recipes/new" element={<RecipeForm handleAddRecipe={handleAddRecipe} />} />
-          <Route path="/my-recipes" element={<RecipeList recipes={recipes} toggleLike={toggleLike} followingIds={followingIds} handleFollow={handleFollow}/>} />
+          <Route path="/my-recipes" element={<RecipeList recipes={recipes.filter((recipe)=> recipe.author._id === userId)} toggleLike={toggleLike} followingIds={followingIds} handleFollow={handleFollow}/>} />
           <Route path="/recipes/:recipeId" element={<RecipeDetails handleDeleteRecipe={handleDeleteRecipe} toggleLike={toggleLike} followingIds={followingIds} handleFollow={handleFollow}  />} />
           <Route path="/recipes/:recipeId/edit" element={<RecipeForm handleUpdateRecipe={handleUpdateRecipe} />} />
           <Route path="/profile" element={<ProfilePage />} />
